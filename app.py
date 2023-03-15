89de4d1e73bf4979062d5d5e082efbafda06f02f
Yuri Rodrigues <122099448+yuriarodrig@users.noreply.github.com>
2023-03-15 10:16:28 -0300

    Update app.py
    Fazendo consulta na minha query e transformando em excel

--- app.py
+++ app.py
@@ -13,3 +13,13 @@ conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' +
                       server+';DATABASE='+database+';UID='+username+';PWD=' + password)
 
 cursor = conn.cursor()
+
+#Minha query para consultar no bd
+query = '''SELECT X6_VAR AS NOME, X6_DESCRIC AS DESCRICAO, X6_DESC1, X6_DESC2  AS DESCRICAO2 FROM SX6010
+WHERE
+
+X6_VAR = 'MV_SUBTRIB' OR X6_VAR = 'MV_STUFS' OR X6_VAR = 'MV_STUF'
+'''
+
+df = pd.read_sql(query, con=conn) #Consulto
+df.to_excel('query sql.xlsx', index=False) #Cria um arquivo Excel
