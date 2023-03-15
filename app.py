52ae7a5d5835c8605c5ddf232185bb896091b65c
Yuri Rodrigues <122099448+yuriarodrig@users.noreply.github.com>
2023-03-15 10:14:31 -0300

    Update app.py
    Fazendo conexão ao BD

--- app.py
+++ app.py
@@ -3,3 +3,13 @@ import pandas as pd
 import pyodbc
 import conect as cn #Importando arquivo de password para a segurança de conexão bd
 
+#Conexão BD 
+server = cn.server
+database = cn.database
+username = cn.username
+password = cn.password
+
+conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' +
+                      server+';DATABASE='+database+';UID='+username+';PWD=' + password)
+
+cursor = conn.cursor()
